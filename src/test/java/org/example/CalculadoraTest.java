1956af1d574baafa3febaf8eb5e1f9bed07b36af
Laura Ardila <laura.ardila.estupinan@gmail.com>
2023-10-04 17:35:16 -0500

    primer commit

---
+++ src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+    Calculadora calculadora;
+
+    @BeforeEach
+    void setCalculadora(){
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    void sumar() {
+        assertEquals(10, calculadora.sumar(6,4));
+    }
+
+    @Test
+    void restar() {
+        assertEquals(2, calculadora.restar(6,4));
+    }
+
+    @Test
+    void multiplicar() {
+        assertEquals(24, calculadora.multiplicar(6,4));
+    }
+}
